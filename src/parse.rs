39ce65fff6df6a148501edaa9f15c2296fb7ba76
[Error_27] <error27dev@gmail.com>
2023-10-10 00:05:10 -0400

    Change to byte pairs rather than strings

--- src/parse.rs
+++ src/parse.rs
@@ -2,11 +2,11 @@ use std::io::{Result, Lines, BufReader, BufRead};
 use std::path::Path;
 use std::fs::File;
 
-pub fn prepare_file<P>(filename: P) -> Vec<String>
+pub fn prepare_file<P>(filename: P) -> Vec<[u8; 2]>
     where P: AsRef<Path>, {
     
     let lines = read_lines(filename).unwrap();
-    let mut parsed: Vec<String> = Vec::new();
+    let mut parsed: Vec<[u8; 2]> = Vec::new();
 
     for line in lines {
         if let Ok(l) = line {
@@ -18,10 +18,10 @@ pub fn prepare_file<P>(filename: P) -> Vec<String>
     parsed
 }
 
-fn convert_owos(input: String) -> Vec<String> {
-    let mut converted: Vec<String> = Vec::new();
+fn convert_owos(input: String) -> Vec<[u8; 2]> {
+    let mut converted: Vec<[u8; 2]> = Vec::new();
     
-    // Convert OwOs to character pairs for actions here
+    // Convert OwOs to byte pairs for actions here
 
     converted
 }
